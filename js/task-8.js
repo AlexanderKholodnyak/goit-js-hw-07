99e5d50cb3d2ad83803d3779609c1fa8ce178841
Alexander Kholodnyak <kholodnyak.a@gmail.com>
2020-10-11 21:53:16 +0300

    8m

--- js/task-8.js
+++ js/task-8.js
@@ -19,6 +19,7 @@
 // </div >
 
 //     <div id="boxes"></div>
+const input = document.querySelector('[type="number"]');
 const render = document.querySelector('[data-action="render"]');
 const destroy = document.querySelector('[data-action="destroy"]');
 const boxes = document.querySelector('#boxes');
@@ -26,16 +27,16 @@ const boxes = document.querySelector('#boxes');
 render.addEventListener('click', getAmount);
 destroy.addEventListener('click', destroyBoxes);
 function getAmount() {
-  let amount = +document.querySelector('#controls input').value;
+  let amount = +input.value;
   createBoxes(amount);
 }
 
 function createBoxes(amount) {
-  let basicSize = 30;
+  const basicSize = 30;
   const fragment = document.createDocumentFragment();
   for (let i = 0; i < amount; i++) {
-    let size = basicSize + i * 10;
-    let div = document.createElement('div');
+    const size = basicSize + i * 10;
+    const div = document.createElement('div');
     div.style.cssText = `width: ${size}px; height: ${size}px; background-color: rgba( ${random()} , ${random()} , ${random()} )`;
     fragment.appendChild(div);
   }
@@ -44,6 +45,7 @@ function createBoxes(amount) {
 
 function destroyBoxes() {
   boxes.innerHTML = '';
+  input.value = null;
 }
 
 function random() {
